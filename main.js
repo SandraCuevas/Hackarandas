14ec183be05bcfaf6bc1bd5b990068f3a79c9b9d
Perla <pataditaenloshuevos@gmail.com>
2019-03-30 17:25:28 -0600

    final casi fanal

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
  function chagePeople(){
       document.getElementById('content').style.display='block';
+      document.getElementById('marco').style.display='block';
       document.getElementById('gif').style.display='none';
-      document.getElementById('btnInicio').style.display='none';
+      document.getElementById('seeMore').style.display='block';
       document.getElementById('personajes').style.display='none';
+     document.getElementById('inicio').style.display='none';
 
    }
 document.getElementById('play').addEventListener('click',chagePeople);
 
+function personajes(){
+  document.getElementById('content').style.display='none';
+  document.getElementById('marco').style.display='none';
+  document.getElementById('gif').style.display='none';
+  document.getElementById('btnInicio').style.display='none';
+  document.getElementById('personajes').style.display='block';
 
-/*function passPage1(){
-  document.getElementById("CHAMPS").style.display="none";//se hace invisible mi pantalla
-  document.getElementById("WELCOME").style.display="block";//muestra el bloque o pantallas
-  document.getElementById("moreInfo").style.display="none";
-  document.getElementById("videoLol").innerHTML=`<iframe id="playVideo" width="800" height="600" src="https://www.youtube.com/embed/BGtROJeMPeE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
 }
-document.getElementById("HOME").addEventListener("click", passPage1);*/
+document.getElementById('seeMore').addEventListener('click',personajes);
