3fa5ba49264de748a9dfa761713009761f289f5d
Sandunga <sandra.cuevastorres@gmail.com>
2019-06-27 09:52:49 -0500

    cambie fondo

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
  function chagePeople(){
       document.getElementById('content').style.display='block';
-      document.getElementById('marco').style.display='block';
       document.getElementById('gif').style.display='none';
       document.getElementById('seeMore').style.display='block';
       document.getElementById('personajes').style.display='none';
      document.getElementById('inicio').style.display='none';
+     document.getElementById('play').style.display='none';
 
    }
 document.getElementById('play').addEventListener('click',chagePeople);
 
 function personajes(){
   document.getElementById('content').style.display='none';
-  document.getElementById('marco').style.display='none';
   document.getElementById('gif').style.display='none';
   document.getElementById('btnInicio').style.display='none';
   document.getElementById('personajes').style.display='block';
-
-
+  document.getElementById('play').style.display='block';
 }
 document.getElementById('seeMore').addEventListener('click',personajes);
